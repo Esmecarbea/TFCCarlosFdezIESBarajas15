77cf1651dc1d2612b76cdfe44a33deb52966088b
Carbea <srfernandez@hotmail.com>
2025-03-17 23:50:45 +0100

    "Versión inicial – botones Pantalla principal bien

---
+++ app/src/main/java/com/tfc/fat13/AccesoActivity.kt
@@ -0,0 +1,11 @@
+package com.tfc.fat13
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AccesoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_acceso) // <-- IMPORTANTE: Enlaza el layout activity_acceso.xml
+    }
+}
--- app/src/main/java/com/tfc/fat13/MainActivity.kt
+++ app/src/main/java/com/tfc/fat13/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tfc.fat13
 
+import com.google.android.material.button.MaterialButton
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -7,5 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val botonAcceso = findViewById<MaterialButton>(R.id.boton_acceso) // <-- Encuentra el botón por su ID
+        botonAcceso.setOnClickListener { // <-- Establece un OnClickListener para el botón
+            // Código a ejecutar cuando se pulse el botón "Acceso"
+            val intent = android.content.Intent(this, AccesoActivity::class.java) // <-- Crea un Intent para iniciar AccesoActivity
+            startActivity(intent) // <-- Inicia la AccesoActivity
+        }
     }
 }
